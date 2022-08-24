0edca67a04e47b56c35765e1b597a4c8068355d8
jianbiaoxu <shiyicz@gmail.com>
2022-08-24 15:40:53 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ envsubst < /root/config.json.tp > /root/config.json
 envsubst '\$PORT' < /root/nginx.template.conf > /root/nginx.conf
 
 # run layui-doc
-v2ray -config /root/config.json & nginx -c /root/nginx.conf
+nginx -c /root/nginx.conf
 
 
