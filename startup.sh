a52e10dc428cdb669282fd4422d5b7c6ff705f68
jianbiaoxu <shiyicz@gmail.com>
2022-08-24 16:29:11 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@
 /usr/bin/envsubst '\$PORT' < /root/nginx.template.conf > /root/nginx.conf
 
 # run layui-doc
-/usr/sbin/nginx -t & /usr/sbin/nginx -c /root/nginx.conf
+/usr/sbin/nginx -t & /usr/sbin/nginx -c /root/nginx.conf -g 'daemon off;'
 
 
